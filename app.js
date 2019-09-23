6f2c16c3705f7a1c7dcb37cb6e4fe013cf425c12
taha uygun <tahauygun@gmail.com>
2019-09-22 22:16:04 -0700

    event cleanup

--- app.js
+++ app.js
@@ -4,10 +4,9 @@ const graphqlHttp = require('express-graphql');
 const { buildSchema } = require('graphql');
 const mongoose = require('mongoose');
 
+const Event = require('./models/event');
 
 const app = express();
-
-const events = [];
 
 app.use(bodyParser.json());
 
@@ -48,15 +47,19 @@ app.use(
         return  events;
       },
       createEvent: (args) => {
-        const event = {
-          _id: Math.random().toString(),
-          title: args.eventInput.title,
-          description: args.eventInput.description,
-          price: +args.eventInput.price,
-          date: args.eventInput.date
-        }
-        events.push(event);
-        return event;
+        const event = new Event({
+            title: args.eventInput.title,
+            description: args.eventInput.description,
+            price: +args.eventInput.price,
+            date: new Date(args.eventInput.date)
+        })
+
+        return event.save().then(result=>{
+          return {...result._doc};
+        }).catch(err=> {
+          console.log(err);
+          throw err;
+        });
       }
     },
     graphiql: true
